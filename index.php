5df0c43b917961585c7ad1f901be1a8df32c83dc
gabriel270613 <gmora9079@gmail.com>
2025-05-18 00:28:30 -0600

    Prueba para ver el Horario

--- index.php
+++ index.php
@@ -192,6 +192,7 @@
             
             <button type="submit" class="btn">Registrar Asesor</button>
         </form>
+        <a href="mostrarhorario.php" class="btn">Ver Horarios</a>
     </main>
 
     <script src="./js/script.js"></script>
